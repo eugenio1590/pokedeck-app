caf2fc14ce2838a613ac161673cdc9f7227905b6
Eugenio Caicedo <eugeniohernandez17@gmail.com>
2024-03-07 21:40:48 -0500

    application component

---
+++ app/javascript/components/Application.js
@@ -0,0 +1,13 @@
+import React from "react"
+import PropTypes from "prop-types"
+
+const Application = (props) => {
+  return (
+    <React.Fragment>
+      Hello World!
+    </React.Fragment>
+  )
+}
+
+
+export default Application
